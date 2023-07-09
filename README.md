7a70ca0798ac1848db167eea6a3621af59604bb6
JB Development <Jsalinas1500@gmail.com>
2023-07-09 01:36:52 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+          ----- For support -----
+---- https://discord.gg/jbdevelopment  ----
+
+# THIS WILL REMOVE ALL UNECESSARY SOUNDS AROUND THE CITY, ESPECIALLY THOSE ANNOYING GUN SHOT SOUNDS IN AMMU-NATION.
+
+# Disable Ambient GTA 5 Sounds.
+# Works with all frameworks.
---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+        ----- For support -----
+---- https://discord.gg/jbdevelopment  ----
+
+--- FX INFORMATION ---
+
+fx_version   'cerulean'
+games        { 'gta5' }
+lua          "54"
+
+-- Resource Information --
+
+author      'JB Development'
+description 'Disable Ambient GTA 5 Sounds'
+version     '1.0.0'
+
+--- Client Information ---
+
+client_script 'client/*.lua'
